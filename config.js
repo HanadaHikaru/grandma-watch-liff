cd7b569eabea893157650491e98fdd702960c8a0
HanadaHikaru <m8625009@s.akita-u.ac.jp>
2026-08-12 07:55:38 +0900

    chore(pages): sync verified LIFF artifact from 8de419354b2a3c24249e3dde4ff9881a36511d97

---
+++ config.js
@@ -0,0 +1,5 @@
+window.GRANDMA_WATCH_CONFIG = {
+  "liffId": "2011073752-fsUt2uPa",
+  "familyApiUrl": "https://hehdsxenhdbtwdtakutk.supabase.co/functions/v1/family-api",
+  "adminApiUrl": "https://hehdsxenhdbtwdtakutk.supabase.co/functions/v1/admin-api"
+};
